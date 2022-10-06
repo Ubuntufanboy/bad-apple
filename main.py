41c84e810317c3cd9acfbb81cc286055043c8267
Ubuntufanboy <82414370+Ubuntufanboy@users.noreply.github.com>
2022-10-06 17:17:10 -0400

    Fixed weird visual glitch in main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ text = []
 for i in range(1, 3286): # There are 1095 frames
     filename = f"new{i}.png"
     with Image.open(filename) as image:
-       frame = ''.join('#' if pix > 50 else ' ' for pix in image.getdata(0))
+       frame = ''.join('#' if pix > 50 else '.' for pix in image.getdata(0))
     text.append(frame)
     if i % 10 == 0:
         print(f"{i}/3285")
@@ -14,10 +14,11 @@ print("Ready to play!")
 from time import sleep
 sleep(1)
 system("clear")
-for i in range(102):
+for i in range(80):
     print("e", end="")
 Silver.play("badapple.mp3")
 for frame in text:
     print(frame, end="")
-    sleep(0.066666)
+    sleep(0.063)
+    system("clear")
 Silver.stop()    
