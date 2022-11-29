338b2ff3daa6841294d6492ea96d945c3367a71b
Ubuntufanboy <82414370+Ubuntufanboy@users.noreply.github.com>
2022-11-28 19:11:54 -0500

    Added 1.2 version support for convert.py
    added a calibration system for the window sizing feature

--- convert.py
+++ convert.py
@@ -4,6 +4,16 @@ from os import system
 from PIL import Image
 num = 1
 # list of all files
+print("Hey! This part of the code breaks a lot so listen up! *press enter to continue*")
+input()
+print("Find the correct size of your terminal. Example sizes are 500x200 or anything like that *press enter to continue*")
+input()
+print("Once you found your terminal size press enter!")
+input()
+x = input("How *long* is your terminal window aka. How many charectors fit on one line: ")
+y = input("How *tall* is your terminal window aka. How many charectors fit vertically in the window: ")
+print("If the program looks weird it's because the values are inncorrect")
+
 def get_file_name(num):
     snum = str(num)
     listed = list(snum)
@@ -20,5 +30,5 @@ def get_file_name(num):
 
 for i in range(1,3286):
     image = Image.open(get_file_name(i))
-    image = image.resize((142,36))
+    image = image.resize((x,y))
     image.save(f'new{i}.png')
