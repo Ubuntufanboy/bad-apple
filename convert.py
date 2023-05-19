f079e4b1c832c3ab151131718aa86c6a15613f5f
Ubuntufanboy <82414370+Ubuntufanboy@users.noreply.github.com>
2023-05-19 18:23:06 -0400

    Change convert.py to use more efficent algo
    removed get_file_name() and using faster Pillow image processing

--- convert.py
+++ convert.py
@@ -1,25 +1,9 @@
-# ./img/output000001.png
-
 from tqdm import tqdm
 import os
 from PIL import Image
 from pathlib import Path
 num = 1
 
-# list of all files
-def get_file_name(num):
-    snum = str(num)
-    listed = list(snum)
-    if len(listed) != 6:
-        amount = 6 - len(listed)
-        new = []
-        for x in range(amount):
-            new.append("0")
-        new.append(snum)
-        ret = ""
-        for number in new:
-            ret += number
-        return f"{current}/img/output{ret}.png"
 
 print("Checking... Do not resize the window!")
 x = os.get_terminal_size().columns
@@ -32,11 +16,11 @@ print("Verdict: ", end="")
 skip = False
 exists = False
 
-p = Path(f"{current}/converted/{x}x{y}") # The path that the frames would go
+p = Path(f"{current}/converted/{x}x{y}")  # The path that the frames would go
 
 if p.exists():
     exists = True
-    
+
     length = sum(1 for x in p.glob('*') if x.is_file())
     if length >= 3284:
         print("Already exists")
@@ -53,16 +37,16 @@ if skip is False:
         try:
             os.chdir("converted")
         except Exception as e:
-            print("Something went wrong when trying to enter converted directory")
+            print("Something went wrong trying to enter converted directory")
             print(e)
         os.mkdir(f"{x}x{y}")
         os.chdir(f"{x}x{y}")
     else:
         os.chdir("converted")
         os.chdir(f"{x}x{y}")
-    for i in tqdm(range(1,3286)):
-        image = Image.open(get_file_name(i))
-        image = image.resize((x,y))
+    for i in tqdm(range(1, 3286)):
+        image = Image.open(f"{current}/img/{i}.png")
+        image = image.resize((x, y), Image.Resampling.NEAREST)
         image.save(f'new{i}.png')
 else:
     print("Converted files already exist! Good to go!")
