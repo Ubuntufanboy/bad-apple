f749ce40dc5edb4b0331d5cd297b9cd38eda2fac
Ubuntufanboy <82414370+Ubuntufanboy@users.noreply.github.com>
2026-02-07 10:41:21 -0500

    Remove upgrade command and add numpy installation

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 sudo apt-get update -y
-sudo apt-get upgrade -y
 sudo apt install python3-brotli -y
 sudo apt install yt-dlp -y
 sudo apt install ffmpeg -y
 pip3 install pillow
+pip3 install numpy
 pip3 install playsound
 pip3 install tqdm
 pip3 install pygame
